3d395e0b944a221b239e4331c979be328d3464be
Miguel Angel <0619851753@uma.es>
2020-12-12 18:58:37 +0100

    Añadir Eventos

---
+++ Eventos.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Asuma
+{
+    partial class Eventos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Eventos));
+            this.bMyEvents = new System.Windows.Forms.Button();
+            this.menuFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.bNoticias = new System.Windows.Forms.Button();
+            this.bEventos = new System.Windows.Forms.Button();
+            this.bInfo = new System.Windows.Forms.Button();
+            this.bContacto = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.lUsername = new System.Windows.Forms.Label();
+            this.pUser = new System.Windows.Forms.PictureBox();
+            this.linitSesion = new System.Windows.Forms.LinkLabel();
+            this.lSignOut = new System.Windows.Forms.LinkLabel();
+            this.panelEventos = new System.Windows.Forms.Panel();
+            this.pASUMA = new System.Windows.Forms.PictureBox();
+            this.pASM = new System.Windows.Forms.PictureBox();
+            this.menuFlowLayoutPanel.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pUser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pASUMA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pASM)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bMyEvents
+            // 
+            this.bMyEvents.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.bMyEvents.Location = new System.Drawing.Point(1192, 718);
+            this.bMyEvents.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bMyEvents.Name = "bMyEvents";
+            this.bMyEvents.Size = new System.Drawing.Size(169, 84);
+            this.bMyEvents.TabIndex = 1;
+            this.bMyEvents.Text = "Mis eventos";
+            this.bMyEvents.UseVisualStyleBackColor = true;
+            this.bMyEvents.Click += new System.EventHandler(this.bMyEvents_Click);
+            this.bMyEvents.Paint += new System.Windows.Forms.PaintEventHandler(this.bMyEvents_Paint);
+            // 
+            // menuFlowLayoutPanel
+            // 
+            this.menuFlowLayoutPanel.BackColor = System.Drawing.Color.Transparent;
+            this.menuFlowLayoutPanel.Controls.Add(this.bNoticias);
+            this.menuFlowLayoutPanel.Controls.Add(this.bEventos);
+            this.menuFlowLayoutPanel.Controls.Add(this.bInfo);
+            this.menuFlowLayoutPanel.Controls.Add(this.bContacto);
+            this.menuFlowLayoutPanel.Controls.Add(this.flowLayoutPanel1);
+            this.menuFlowLayoutPanel.Controls.Add(this.checkBox1);
+            this.menuFlowLayoutPanel.Location = new System.Drawing.Point(8, 98);
+            this.menuFlowLayoutPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.menuFlowLayoutPanel.Name = "menuFlowLayoutPanel";
+            this.menuFlowLayoutPanel.Size = new System.Drawing.Size(1341, 54);
+            this.menuFlowLayoutPanel.TabIndex = 24;
+            this.menuFlowLayoutPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.menuFlowLayoutPanel_Paint);
+            // 
+            // bNoticias
+            // 
+            this.bNoticias.Location = new System.Drawing.Point(3, 2);
+            this.bNoticias.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.bNoticias.Name = "bNoticias";
+            this.bNoticias.Size = new System.Drawing.Size(127, 46);
+            this.bNoticias.TabIndex = 0;
+            this.bNoticias.Text = "Noticias";
+            this.bNoticias.UseVisualStyleBackColor = true;
+            // 
+            // bEventos
+            // 
+            this.bEventos.Location = new System.Drawing.Point(136, 2);
+            this.bEventos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.bEventos.Name = "bEventos";
+            this.bEventos.Size = new System.Drawing.Size(127, 46);
+            this.bEventos.TabIndex = 1;
+            this.bEventos.Text = "Eventos";
+            this.bEventos.UseVisualStyleBackColor = true;
+            // 
+            // bInfo
+            // 
+            this.bInfo.Location = new System.Drawing.Point(269, 2);
+            this.bInfo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.bInfo.Name = "bInfo";
+            this.bInfo.Size = new System.Drawing.Size(127, 46);
+            this.bInfo.TabIndex = 2;
+            this.bInfo.Text = "Información";
+            this.bInfo.UseVisualStyleBackColor = true;
+            // 
+            // bContacto
+            // 
+            this.bContacto.Location = new System.Drawing.Point(402, 2);
+            this.bContacto.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.bContacto.Name = "bContacto";
+            this.bContacto.Size = new System.Drawing.Size(127, 46);
+            this.bContacto.TabIndex = 3;
+            this.bContacto.Text = "Contacto";
+            this.bContacto.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 52);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1405, 1024);
+            this.flowLayoutPanel1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Noticias";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(136, 2);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Eventos";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(269, 2);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 46);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Información";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(402, 2);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 46);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Contacto";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 1080);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // lUsername
+            // 
+            this.lUsername.AutoSize = true;
+            this.lUsername.Location = new System.Drawing.Point(124, 46);
+            this.lUsername.Name = "lUsername";
+            this.lUsername.Size = new System.Drawing.Size(73, 17);
+            this.lUsername.TabIndex = 23;
+            this.lUsername.Text = "userName";
+            // 
+            // pUser
+            // 
+            this.pUser.Location = new System.Drawing.Point(13, 12);
+            this.pUser.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pUser.Name = "pUser";
+            this.pUser.Size = new System.Drawing.Size(80, 78);
+            this.pUser.TabIndex = 22;
+            this.pUser.TabStop = false;
+            // 
+            // linitSesion
+            // 
+            this.linitSesion.AutoSize = true;
+            this.linitSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linitSesion.Location = new System.Drawing.Point(152, 41);
+            this.linitSesion.Name = "linitSesion";
+            this.linitSesion.Size = new System.Drawing.Size(119, 24);
+            this.linitSesion.TabIndex = 28;
+            this.linitSesion.TabStop = true;
+            this.linitSesion.Text = "Iniciar sesion";
+            this.linitSesion.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linitSesion_LinkClicked);
+            // 
+            // lSignOut
+            // 
+            this.lSignOut.AutoSize = true;
+            this.lSignOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lSignOut.Location = new System.Drawing.Point(277, 43);
+            this.lSignOut.Name = "lSignOut";
+            this.lSignOut.Size = new System.Drawing.Size(111, 20);
+            this.lSignOut.TabIndex = 29;
+            this.lSignOut.TabStop = true;
+            this.lSignOut.Text = "Cerrar sesión";
+            this.lSignOut.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lSignOut_LinkClicked);
+            // 
+            // panelEventos
+            // 
+            this.panelEventos.AutoScroll = true;
+            this.panelEventos.AutoScrollMargin = new System.Drawing.Size(50, 50);
+            this.panelEventos.Location = new System.Drawing.Point(440, 176);
+            this.panelEventos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panelEventos.Name = "panelEventos";
+            this.panelEventos.Size = new System.Drawing.Size(1247, 519);
+            this.panelEventos.TabIndex = 31;
+            // 
+            // pASUMA
+            // 
+            this.pASUMA.InitialImage = null;
+            this.pASUMA.Location = new System.Drawing.Point(497, 14);
+            this.pASUMA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pASUMA.Name = "pASUMA";
+            this.pASUMA.Size = new System.Drawing.Size(389, 76);
+            this.pASUMA.TabIndex = 32;
+            this.pASUMA.TabStop = false;
+            this.pASUMA.Paint += new System.Windows.Forms.PaintEventHandler(this.pASUMA_Paint);
+            // 
+            // pASM
+            // 
+            this.pASM.Image = ((System.Drawing.Image)(resources.GetObject("pASM.Image")));
+            this.pASM.InitialImage = null;
+            this.pASM.Location = new System.Drawing.Point(967, 14);
+            this.pASM.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pASM.Name = "pASM";
+            this.pASM.Size = new System.Drawing.Size(309, 76);
+            this.pASM.TabIndex = 33;
+            this.pASM.TabStop = false;
+            this.pASM.Paint += new System.Windows.Forms.PaintEventHandler(this.pASM_Paint);
+            // 
+            // Eventos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1368, 855);
+            this.Controls.Add(this.panelEventos);
+            this.Controls.Add(this.pASM);
+            this.Controls.Add(this.pASUMA);
+            this.Controls.Add(this.bMyEvents);
+            this.Controls.Add(this.lSignOut);
+            this.Controls.Add(this.menuFlowLayoutPanel);
+            this.Controls.Add(this.lUsername);
+            this.Controls.Add(this.pUser);
+            this.Controls.Add(this.linitSesion);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Eventos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Eventos";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuFlowLayoutPanel.ResumeLayout(false);
+            this.menuFlowLayoutPanel.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pUser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pASUMA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pASM)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button bMyEvents;
+        private System.Windows.Forms.FlowLayoutPanel menuFlowLayoutPanel;
+        private System.Windows.Forms.Button bNoticias;
+        private System.Windows.Forms.Button bEventos;
+        private System.Windows.Forms.Button bInfo;
+        private System.Windows.Forms.Button bContacto;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label lUsername;
+        private System.Windows.Forms.PictureBox pUser;
+        private System.Windows.Forms.LinkLabel linitSesion;
+        private System.Windows.Forms.LinkLabel lSignOut;
+        private System.Windows.Forms.Panel panelEventos;
+        private System.Windows.Forms.PictureBox pASUMA;
+        private System.Windows.Forms.PictureBox pASM;
+    }
+}
---
+++ Eventos.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+using System.Resources;
+using System.IO;
+
+namespace Asuma
+{
+    public partial class Eventos : Form
+    {
+        private User usuario;
+        public Eventos(User usuario)
+        {
+            InitializeComponent();
+            this.usuario = usuario;
+            actualizar();
+            panelEventos.SendToBack();
+            mostrarEventos();
+        }
+
+        private void menuFlowLayoutPanel_Paint(object sender, PaintEventArgs e)
+        {
+            this.menuFlowLayoutPanel.Width = this.Width - 25;
+            this.bNoticias.Width = this.menuFlowLayoutPanel.Width / 4 - 10;
+            this.bEventos.Width = this.menuFlowLayoutPanel.Width / 4 - 10;
+            this.bInfo.Width = this.menuFlowLayoutPanel.Width / 4 - 10;
+            this.bContacto.Width = this.menuFlowLayoutPanel.Width / 4 - 10;
+        }
+
+        private void bMyEvents_Click(object sender, EventArgs e)
+        {
+            if (usuario == null)
+            {
+                MessageBox.Show("Debe iniciar sesión para mostrar sus eventos");
+            }
+            else
+            {
+                MessageBox.Show("Si eres USUARIO");
+            }
+        }
+
+        public void actualizar()
+        {
+            if (usuario == null)
+            {
+                linitSesion.Visible = true;
+                pUser.Visible = false;
+                lUsername.Visible = false;
+                lSignOut.Visible = false;
+            }
+            else
+            {
+                linitSesion.Visible = false;
+                pUser.Visible = true;
+                lUsername.Text = "Bienvenido, " + usuario.Username;
+                lUsername.Visible = true;
+                lSignOut.Visible = true;
+            }
+        }
+
+        private void linitSesion_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Inicio init = new Inicio();
+            this.Visible = false;
+            init.ShowDialog();
+            this.usuario = Inicio.usuario;
+            actualizar();
+            this.Visible = true;
+        }
+
+        private void lSignOut_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void mostrarEventos()
+        {
+            List<Event> listaEventos = Event.listaEventos();
+            int nEventos = listaEventos.Count;
+            int separacion = 50;
+
+            for (int i = 0; i < nEventos; i++)
+            {
+
+                string eventName = listaEventos.ElementAt(i).EventName;
+                string eventDate = listaEventos.ElementAt(i).Date;
+                string eventDescription = listaEventos.ElementAt(i).EventDescription;
+                string imagen = listaEventos.ElementAt(i).Image;
+                int id = listaEventos.ElementAt(i).ID;
+
+                Panel panel = new Panel();
+                panel.Name = "pEvento" + id;
+
+                //------------------
+
+                LinkLabel ltitulo = new LinkLabel();
+                ltitulo.Text = eventName;
+                ltitulo.Size = new Size(292, 38);
+                ltitulo.AutoSize = true;
+                ltitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                ltitulo.LinkColor = System.Drawing.Color.Black;
+                ltitulo.Location = new System.Drawing.Point(230, 18);
+                ltitulo.Name = id.ToString();
+                ltitulo.TabIndex = 31;
+                ltitulo.TabStop = true;
+                ltitulo.Visible = true;
+                ltitulo.Click += new EventHandler(ltitulo_click);
+
+                //------------------
+
+
+                PictureBox pImagen = new PictureBox();
+                pImagen.BackColor = System.Drawing.SystemColors.ActiveCaption;
+                pImagen.Location = new System.Drawing.Point(59, 28);
+                pImagen.Name = "pImagen";
+                string path = Path.GetDirectoryName(Application.StartupPath);
+                string pathBueno = path.Substring(0, path.Length - 3);
+                string imagePath = pathBueno + "images\\" + imagen;
+                Image image = Image.FromFile(imagePath);
+                pImagen.Image = image;
+                pImagen.SizeMode = PictureBoxSizeMode.StretchImage;
+                pImagen.Size = new System.Drawing.Size(115, 127);
+                pImagen.TabIndex = 0;
+                pImagen.TabStop = false;
+                pImagen.Visible = true;
+                pImagen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+
+
+                TextBox tFecha = new TextBox();
+                tFecha.Text = "Fecha del evento: " + eventDate;
+                tFecha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+                tFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                tFecha.Location = new System.Drawing.Point(900, 145);
+                tFecha.Multiline = true;
+                tFecha.Name = "tFecha";
+                tFecha.ReadOnly = true;
+                tFecha.Size = new System.Drawing.Size(216, 24);
+                tFecha.TabIndex = 32;
+                tFecha.Visible = true;
+
+
+
+                panel.Size = new Size(1142, 186); //1142 186
+
+                panel.Location = new Point(50, separacion);
+                panel.Visible = true;
+                panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+
+
+                TextBox descripcion = new TextBox();
+                descripcion.Text = eventDescription;
+                descripcion.BorderStyle = System.Windows.Forms.BorderStyle.None;
+                descripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                descripcion.Location = new System.Drawing.Point(233, 68);
+                descripcion.Multiline = true;
+                descripcion.Name = "tDescripcion";
+                descripcion.ReadOnly = true;
+                descripcion.Size = new System.Drawing.Size(panel.Width / 2, panel.Height / 2); //685,59
+                descripcion.TabIndex = 2;
+                descripcion.Visible = true;
+
+
+
+                panel.Controls.Add(ltitulo);
+                panel.Controls.Add(pImagen);
+                panel.Controls.Add(tFecha);
+                panel.Controls.Add(descripcion);
+                panelEventos.Controls.Add(panel);
+                separacion += 180;
+            }
+            panelEventos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            panelEventos.Size = new Size(1265, 640);
+            //panelEventos.Location = new Point(this.Width - panelEventos.Width/2, panelEventos.Location.Y);
+        }
+        protected void ltitulo_click(object sender, EventArgs e)
+        {
+            LinkLabel link = sender as LinkLabel;
+            // identify which button was clicked and perform necessary actions
+            var id = Int32.Parse(link.Name);
+            Event ev = new Event(id);
+            InfoEvento infoEvento = new InfoEvento(ev,null);
+            this.Visible = false;
+            infoEvento.ShowDialog();
+            this.Visible = true;
+        }
+
+        private void bMyEvents_Paint(object sender, PaintEventArgs e)
+        {
+            bMyEvents.Location = new Point(this.Width / 2 - 90, 850);
+        }
+
+        private void pASUMA_Paint(object sender, PaintEventArgs e)
+        {
+            this.pASUMA.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void pASM_Paint(object sender, PaintEventArgs e)
+        {
+            this.pASM.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
