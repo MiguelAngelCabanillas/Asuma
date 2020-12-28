c3ab9bcfb0477d66e6f20f839eb87fad3f15f33e
Francisco Maria Bono Navarro <franbono2@alu.uma.es>
2020-12-28 20:28:15 +0100

    informacion

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Asuma
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Principal(null));
+            Application.Run(new Informacion(null));
             // new User("docente1", "d"))
         }
     }
